f97dd0bed249f4eea2a3c02a5738d71f312d4be4
Harry Nguon <harrynguon@gmail.com>
2021-09-26 01:50:51 +1200

    Update item functionality has been implemented, undo functionality as well

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -10,85 +10,40 @@ import {
 	Snackbar,
 	Subheading,
 	Text,
+	TextInput,
 	Title,
-	TouchableRipple,
 } from 'react-native-paper';
 
 const RightSide = (props) => {
-	console.log(props.dateCreated.toLocaleString());
-
-	// var now = props.momentCreated.add(36, 'hours');
-	// var b = moment();
-	// var diff_s = now.diff(b, 'seconds');
-	// var diff_m = now.diff(b, 'minutes');
-	// var diff_h = now.diff(b, 'hours');
-	//
-	// let individualTimeUnit =
-	// 	diff_h >= 1
-	// 		? `${diff_h} hours remaining.`
-	// 		: diff_m >= 1
-	// 		? `${diff_m} minutes remaining.`
-	// 		: `${diff_s} seconds remaining.`;
-
-	// let timehhmmss = `${moment.utc(diff_s * 1000).format('HH:mm:ss')} remaining.`;
-	//
-	// if (diff_h >= 24) {
-	// 	let numHours = timehhmmss.split(':')[0];
-	// 	let correctHours = (Number.parseInt(numHours) + 24).toString();
-	// 	let numMinutes = timehhmmss.split(':')[1] ?? '00';
-	// 	let numSeconds = timehhmmss.split(':')[2] ?? '00';
-	// 	timehhmmss = `${correctHours}:${numMinutes}:${numSeconds}`;
-	// }
-
-	// let timeRemainingString = props.useTimestamp ? timehhmmss : individualTimeUnit;
-
 	return (
 		<View style={styles.listRightSide}>
-			<List.Icon {...props} icon={'clock-time-ten'} key={Math.random()} />
-			<Text>{props.dateCreated.toLocaleString()}</Text>
+			<Text>Valid Until: </Text>
+			<Text>{props.expiryDate.toLocaleString()}</Text>
 		</View>
 	);
 };
 
 const HomeScreen = () => {
-	const [items, setItems] = useState([
-		{
-			title: 'First',
-			description: 'Hello this is first item',
-			dateCreated: new Date(),
-		},
-		// {
-		// 	title: 'Second',
-		// 	description: 'Yes Sir',
-		// 	momentCreated: moment().subtract(3, 'hours'),
-		// },
-		// {
-		// 	title: 'Third',
-		// 	description: 'Testing',
-		// 	momentCreated: moment().subtract(5, 'hours').subtract(10, 'minutes'),
-		// },
-		// {
-		// 	title: 'Fourth',
-		// 	description: 'No',
-		// 	momentCreated: moment().subtract(7, 'hours'),
-		// },
-		// {
-		// 	title: 'Fifth',
-		// 	description: 'Hi XD',
-		// 	momentCreated: moment().subtract(30, 'hours'),
-		// },
-	]);
+	const [items, setItems] = useState([]);
 
-	const [visible, setVisible] = useState(false);
-	const onToggleSnackBar = () => setVisible(!visible);
-	const onDismissSnackBar = () => setVisible(false);
+	const [snackbarVisible, setSnackbarVisible] = useState(false);
+	// const onToggleSnackBar = () => setVisible(!visible);
+	const onDismissSnackBar = () => setSnackbarVisible(false);
 
 	const [modalVisible, setModalVisible] = useState(false);
-	const modalVisibleSet = () => setModalVisible(true);
 
-	console.log(modalVisible);
+	const [inputText, setInputText] = useState('');
+	const [listItemReference, setListItemReference] = useState(null);
 
-	// const [useTimestamp, toggleTimestamp] = useState(false);
+	const [snapshot, setSnapshot] = useState();
+
+	const handleUpdateText = (text, editing = true) => {
+		let itemsCopy = [...items];
+		let referenceIndex = itemsCopy.findIndex((item) => item === listItemReference);
+		itemsCopy[referenceIndex]['description'] = text;
+		setInputText(text);
+		setItems(itemsCopy);
+	};
 
 	return (
 		<View style={[styles.container, styles.maxWidth]}>
@@ -104,28 +59,42 @@ const HomeScreen = () => {
 			<View style={[styles.contentContainer, styles.maxWidth]}>
 				{items.map((item) => (
 					<List.Item
-						style={{ backgroundColor: 'green', width: '100%' }}
-						title={item.title}
+						style={{
+							backgroundColor: 'green',
+							width: '100%',
+							padding: '15px',
+							margin: 3,
+						}}
 						description={item.description}
-						onPress={setModalVisible}
+						onPress={(event) => {
+							setModalVisible(true);
+							setInputText(item.description);
+							setListItemReference(item);
+							setSnapshot(item.description);
+						}}
 						right={(props) => (
-							<RightSide
-								props={props}
-								dateCreated={item.dateCreated}
-								// useTimestamp={useTimestamp}
-								// toggleTimestampFunction={toggleTimestamp}
-								key={Math.random()}
-							/>
+							<RightSide props={props} expiryDate={item.expiryDate} key={Math.random()} />
 						)}
 					/>
 				))}
 				<Portal>
 					<Modal
 						visible={modalVisible}
-						onDismiss={(event) => setModalVisible(false)}
-						contentContainerStyle={{ backgroundColor: 'orange', padding: 20 }}
+						onDismiss={(event) => {
+							setModalVisible(false);
+							if (snapshot !== listItemReference.description) {
+								setSnackbarVisible(true);
+							}
+						}}
+						contentContainerStyle={{ backgroundColor: 'blue', padding: 20 }}
 					>
-						<Text>Example Modal!</Text>
+						<TextInput
+							label="Modify Text"
+							value={inputText}
+							onChangeText={(text) => {
+								handleUpdateText(text);
+							}}
+						/>
 					</Modal>
 				</Portal>
 			</View>
@@ -134,12 +103,16 @@ const HomeScreen = () => {
 				<Caption style={{ marginTop: -15, marginBottom: 15 }}>{items.length} / 5</Caption>
 				<Portal>
 					<Snackbar
-						visible={visible}
+						visible={snackbarVisible}
 						onDismiss={onDismissSnackBar}
-						duration={4000}
+						duration={3000}
 						action={{
 							label: 'Undo',
-							onPress: () => {},
+							onPress: (event) => {
+								if (snapshot !== listItemReference.description) {
+									handleUpdateText(snapshot, false);
+								}
+							},
 						}}
 					>
 						You have deleted item {'hi'}.
@@ -147,17 +120,22 @@ const HomeScreen = () => {
 				</Portal>
 				<Button
 					icon={'home'}
-					disabled={items.length === 5}
-					onPress={(event) =>
-						setItems([
-							...items,
-							{
-								title: 'First',
-								description: 'Hello this is first item',
-								dateCreated: new Date(),
-							},
-						])
-					}
+					disabled={items.length >= 5}
+					onPress={(event) => {
+						let itemsCopy = [...items];
+						itemsCopy.push({
+							description: 'Hello this is first item',
+							expiryDate: new Date(new Date().setHours(new Date().getHours() + 36)),
+						});
+						// setItems([
+						// 	...items,
+						// 	{
+						// 		description: 'Hello this is first item',
+						// 		expiryDate: new Date(new Date().setHours(new Date().getHours() + 36)),
+						// 	},
+						// ]);
+						setItems(itemsCopy);
+					}}
 				>
 					Add
 				</Button>
