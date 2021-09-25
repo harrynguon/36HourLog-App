d4a29048b3767863b13c43ce79ec9b50aeae3149
Harry Nguon <harrynguon@gmail.com>
2021-09-26 00:42:28 +1200

    Moment.js causing issues upon state update... Removed it

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -4,123 +4,130 @@ import {
 	Button,
 	Caption,
 	List,
+	Modal,
 	Paragraph,
 	Portal,
 	Snackbar,
 	Subheading,
 	Text,
 	Title,
+	TouchableRipple,
 } from 'react-native-paper';
-import moment from 'moment';
-import { TouchableOpacity } from 'react-native-web';
-
-const items = [
-	{
-		title: 'First',
-		description: 'Hello this is first item',
-		momentCreated: moment(),
-	},
-	{
-		title: 'Second',
-		description: 'Yes Sir',
-		momentCreated: moment().subtract(3, 'hours'),
-	},
-	{
-		title: 'Third',
-		description: 'Testing',
-		momentCreated: moment().subtract(5, 'hours').subtract(10, 'minutes'),
-	},
-	{
-		title: 'Fourth',
-		description: 'No',
-		momentCreated: moment().subtract(7, 'hours'),
-	},
-	{
-		title: 'Fifth',
-		description: 'Hi XD',
-		momentCreated: moment().subtract(30, 'hours'),
-	},
-];
 
 const RightSide = (props) => {
-	var now = props.momentCreated.add(36, 'hours');
-	var b = moment();
-	var diff_s = now.diff(b, 'seconds');
-	var diff_m = now.diff(b, 'minutes');
-	var diff_h = now.diff(b, 'hours');
+	console.log(props.dateCreated.toLocaleString());
 
-	let individualTimeUnit =
-		diff_h >= 1
-			? `${diff_h} hours remaining.`
-			: diff_m >= 1
-			? `${diff_m} minutes remaining.`
-			: `${diff_s} seconds remaining.`;
+	// var now = props.momentCreated.add(36, 'hours');
+	// var b = moment();
+	// var diff_s = now.diff(b, 'seconds');
+	// var diff_m = now.diff(b, 'minutes');
+	// var diff_h = now.diff(b, 'hours');
+	//
+	// let individualTimeUnit =
+	// 	diff_h >= 1
+	// 		? `${diff_h} hours remaining.`
+	// 		: diff_m >= 1
+	// 		? `${diff_m} minutes remaining.`
+	// 		: `${diff_s} seconds remaining.`;
 
-	let timehhmmss = `${moment.utc(diff_s * 1000).format('HH:mm:ss')} remaining.`;
+	// let timehhmmss = `${moment.utc(diff_s * 1000).format('HH:mm:ss')} remaining.`;
+	//
+	// if (diff_h >= 24) {
+	// 	let numHours = timehhmmss.split(':')[0];
+	// 	let correctHours = (Number.parseInt(numHours) + 24).toString();
+	// 	let numMinutes = timehhmmss.split(':')[1] ?? '00';
+	// 	let numSeconds = timehhmmss.split(':')[2] ?? '00';
+	// 	timehhmmss = `${correctHours}:${numMinutes}:${numSeconds}`;
+	// }
 
-	if (diff_h >= 24) {
-		let numHours = timehhmmss.split(':')[0];
-		let correctHours = (Number.parseInt(numHours) + 24).toString();
-		let numMinutes = timehhmmss.split(':')[1] ?? '00';
-		let numSeconds = timehhmmss.split(':')[2] ?? '00';
-		timehhmmss = `${correctHours}:${numMinutes}:${numSeconds}`;
-	}
-
-	let timeRemainingString = props.useTimestamp ? timehhmmss : individualTimeUnit;
+	// let timeRemainingString = props.useTimestamp ? timehhmmss : individualTimeUnit;
 
 	return (
 		<View style={styles.listRightSide}>
-			<TouchableOpacity onPress={props.toggleTimestampFunction}>
-				<List.Icon {...props} icon={'clock-time-ten'} key={Math.random()} />
-				<Text>{timeRemainingString}</Text>
-			</TouchableOpacity>
+			<List.Icon {...props} icon={'clock-time-ten'} key={Math.random()} />
+			<Text>{props.dateCreated.toLocaleString()}</Text>
 		</View>
 	);
 };
 
-function useToggle(initialValue = false) {
-	const [value, setValue] = useState(initialValue);
-
-	const toggle = React.useCallback(() => {
-		setValue((value) => !value);
-	}, []);
-
-	return [value, toggle];
-}
-
 const HomeScreen = () => {
-	const [visible, setVisible] = React.useState(false);
+	const [items, setItems] = useState([
+		{
+			title: 'First',
+			description: 'Hello this is first item',
+			dateCreated: new Date(),
+		},
+		// {
+		// 	title: 'Second',
+		// 	description: 'Yes Sir',
+		// 	momentCreated: moment().subtract(3, 'hours'),
+		// },
+		// {
+		// 	title: 'Third',
+		// 	description: 'Testing',
+		// 	momentCreated: moment().subtract(5, 'hours').subtract(10, 'minutes'),
+		// },
+		// {
+		// 	title: 'Fourth',
+		// 	description: 'No',
+		// 	momentCreated: moment().subtract(7, 'hours'),
+		// },
+		// {
+		// 	title: 'Fifth',
+		// 	description: 'Hi XD',
+		// 	momentCreated: moment().subtract(30, 'hours'),
+		// },
+	]);
+
+	const [visible, setVisible] = useState(false);
 	const onToggleSnackBar = () => setVisible(!visible);
 	const onDismissSnackBar = () => setVisible(false);
 
-	const [useTimestamp, toggleTimestamp] = useToggle(false);
+	const [modalVisible, setModalVisible] = useState(false);
+	const modalVisibleSet = () => setModalVisible(true);
+
+	console.log(modalVisible);
+
+	// const [useTimestamp, toggleTimestamp] = useState(false);
 
 	return (
 		<View style={[styles.container, styles.maxWidth]}>
 			<View style={[styles.headerContainer, styles.maxWidth]}>
 				<Title style={{ textDecorationLine: 'underline' }}>36 Hour Logger</Title>
 				<Subheading>Log anything you want!</Subheading>
-				<Paragraph style={{ fontSize: 10, fontStyle: 'italic' }}>
-					You can log up to 5 items. Swipe an item to delete it.
+				<Paragraph style={{ textAlign: 'center', fontSize: 10, fontStyle: 'italic' }}>
+					You can log up to 5 items. Swipe an item to delete it. Items are automatically
+					deleted after 36 hours.
 				</Paragraph>
 			</View>
+
 			<View style={[styles.contentContainer, styles.maxWidth]}>
 				{items.map((item) => (
 					<List.Item
 						style={{ backgroundColor: 'green', width: '100%' }}
 						title={item.title}
 						description={item.description}
+						onPress={setModalVisible}
 						right={(props) => (
 							<RightSide
 								props={props}
-								momentCreated={item.momentCreated}
-								useTimestamp={useTimestamp}
-								toggleTimestampFunction={toggleTimestamp}
+								dateCreated={item.dateCreated}
+								// useTimestamp={useTimestamp}
+								// toggleTimestampFunction={toggleTimestamp}
 								key={Math.random()}
 							/>
 						)}
 					/>
 				))}
+				<Portal>
+					<Modal
+						visible={modalVisible}
+						onDismiss={(event) => setModalVisible(false)}
+						contentContainerStyle={{ backgroundColor: 'orange', padding: 20 }}
+					>
+						<Text>Example Modal!</Text>
+					</Modal>
+				</Portal>
 			</View>
 
 			<View style={[styles.footerContainer, styles.maxWidth]}>
@@ -138,7 +145,20 @@ const HomeScreen = () => {
 						You have deleted item {'hi'}.
 					</Snackbar>
 				</Portal>
-				<Button icon={'home'} disabled={items.length === 5}>
+				<Button
+					icon={'home'}
+					disabled={items.length === 5}
+					onPress={(event) =>
+						setItems([
+							...items,
+							{
+								title: 'First',
+								description: 'Hello this is first item',
+								dateCreated: new Date(),
+							},
+						])
+					}
+				>
 					Add
 				</Button>
 			</View>
